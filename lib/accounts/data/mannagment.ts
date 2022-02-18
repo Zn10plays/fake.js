5e14d5100458aa4baab6b6b3e4d2127532cf5fc1
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2022-02-18 13:33:34 -0500

    simple code

--- lib/accounts/data/mannagment.ts
+++ lib/accounts/data/mannagment.ts
@@ -9,6 +9,24 @@ class Manager {
     const lNames: String[] = data.lastName
     return lNames[Math.floor(Math.random() * lNames.length)]
   }
+  static get getRandomNumber(opt: {style: boolean}): String | number {
+    let length: number = 8,
+      charset: String = "1234567890",
+      retVal: String = "";
+    
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    if (opt.style) {
+      return '+1 ('+ retVal.slice(0, 3) + ')  ' + retVal.slice(3, 6) + ' - ' + retVal.slice(6, 10)
+    }
+    return retVal;
+  }
+
+  static get getRandomPassword(opt: {length: number, number: boolean, letters: boolean, specialCharacters: String[]}) {
+    
+  }
 }
+
 
 export default Manager
