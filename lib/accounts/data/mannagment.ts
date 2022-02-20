6d544010d3bc1b2dccd5525476a4bbfd353e551a
Rudra Patel <rudrapatel26122004@gmail.com>
2022-02-19 20:59:27 -0500

    error fixes

--- lib/accounts/data/mannagment.ts
+++ lib/accounts/data/mannagment.ts
@@ -1,32 +1,50 @@
 import data from './data.json'
 
 class Manager {
-  static get getRandomFirstName(): String {
+  static get getRandomFirstName (): String {
     const fNames = data.firstNames
     return fNames[Math.floor(Math.random() * fNames.length)]
   }
-  static get getRandomLastName(): String {
+
+  static get getRandomLastName (): String {
     const lNames: String[] = data.lastName
     return lNames[Math.floor(Math.random() * lNames.length)]
   }
-  static get getRandomNumber(opt: {style: boolean}): String | number {
-    let length: number = 8,
-      charset: String = "1234567890",
-      retVal: String = "";
-    
+
+  static getRandomNumber (opt: {style: boolean}): String {
+    const length: number = 8
+    const charset: String = '1234567890'
+    const retVal: String[] = []
+
     for (let i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+      retVal.push(charset.charAt(Math.floor(Math.random() * n)))
     }
+
+    const numstr = retVal.join()
     if (opt.style) {
-      return '+1 ('+ retVal.slice(0, 3) + ')  ' + retVal.slice(3, 6) + ' - ' + retVal.slice(6, 10)
+      return '+1 (' + numstr.slice(0, 3) + ')  ' + numstr.slice(3, 6) + ' - ' + numstr.slice(6, 10)
     }
-    return retVal;
+    return retVal.join()
   }
 
-  static get getRandomPassword(opt: {length: number, number: boolean, letters: boolean, specialCharacters: String[]}) {
-    
+  static getRandomPassword (opt: {length: 8, number: false, capital: true, specialCharacters: String[]}): String {
+    let pass: string = ''
+    const nums: string = '1234567890'
+    let characters: string = 'qwertyuiopasdfghjklzxcvbnm'
+    const capitals: string = 'QWERTYUIOOPASDFGHJKLZXCVBNM'
+
+    if (opt.number) {
+      characters += nums
+    }
+    if (opt.capital) {
+      characters += capitals
+    }
+
+    for (let i = 0, n = characters.length; i < 8; ++i) {
+      pass += characters.charAt(Math.floor(Math.random() * n))
+    }
+    return pass
   }
 }
-
 
 export default Manager
