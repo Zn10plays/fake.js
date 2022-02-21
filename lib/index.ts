72bb93d6b9c664c78767193c1908f77c7b8aaedd
Rudra Patel <rudrapatel26122004@gmail.com>
2022-02-21 18:59:09 -0500

    fixed integration issues

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,4 @@
-export * as randAccount from './accounts/accounts'
-export * as randInfo from './accounts/data/managment'
+import randAccount from './accounts/accounts'
+import * as randInfo from './accounts/data/managment'
+
+export { randAccount, randInfo }
